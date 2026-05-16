d052f12ceb94a348ae8cd7075b182beac736b0cb
maozblan <lyssa.li137@gmail.com>
2026-05-16 08:51:12 -0700

    started inputing artist data

---
+++ slugcon2026/artists/artistInfo.js
@@ -0,0 +1,70 @@
+// why is this not a .json? fetch() is too much work
+const artistInfo = {
+	janchi: {
+		name: "jam.chii_",
+		bio: "",
+		artworkCount: 3,
+		socials: {
+			instagram: "https://www.instagram.com/jam.chii_/",
+		},
+	},
+	infatrash: {
+		name: "InfiniteTra5h",
+		bio: "",
+		artworkCount: 4,
+		socials: {
+			instagram: "https://www.instagram.com/infinitetra5h",
+		},
+	},
+	wightfox: {
+		name: "alittlewightfox",
+		bio:
+			"Hey I’m Elia or ALittleWightFox! I’m an amateur artist who’s in it for the love of the game. I’m into a lot of mainstream stuff but also like to play into niches a little. If you see me come say hi!",
+		artworkCount: 6,
+		socials: {
+			twitter: "https://x.com/alittlewightfox",
+			tumblr: "https://www.tumblr.com/alittlewightfox",
+			instagram: "https://www.instagram.com/alittlewightfox",
+		},
+	},
+	oerin: {
+		name: "03RIN",
+		bio: "hi im 03RIN, i love drinking matcha and reading fanfiction (๑˃̵ᴗ˂̵)",
+		artworkCount: 4,
+		socials: {
+			twitter: "https://x.com/03rin__",
+		},
+	},
+	steelfirns: {
+		name: "Steelfirns",
+		bio:
+			"I draw pretty pictures on my computer for games, zines, and comics. Often about machines and other monstrous things.",
+		artworkCount: 4,
+		socials: {
+			instagram: "https://www.instagram.com/steelfirns",
+		},
+	},
+	oats: {
+		name: "Oats",
+		bio:
+			`"Hi! I'm Andrew/Oats, a merch artist and game developer :) I like to make keychains based off of my favorite games and anime! I especially like making keychains that are interactive and have weird materials.
+			
+			Reach out if you want to talk about art, game development (especially technical art, which is my craft), fashion, politics, or anything else :3"`,
+		artworkCount: 4,
+		socials: {
+			bluesky: "https://bsky.app/profile/oatvercast.bsky.social",
+		},
+	},
+	// name: {
+	// 	name: "",
+	// 	bio: "",
+	// 	artworkCount: 0,
+	// 	socials: {},
+	// },
+	// name: {
+	// 	name: "",
+	// 	bio: "",
+	// 	artworkCount: 0,
+	// 	socials: {},
+	// },
+};
