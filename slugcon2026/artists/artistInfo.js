77b70f94b07e273119f2f32c71fe3adebead5be2
maozblan <lyssa.li137@gmail.com>
2026-05-17 11:54:43 -0700

    8 more artists >.>

--- slugcon2026/artists/artistInfo.js
+++ slugcon2026/artists/artistInfo.js
@@ -72,9 +72,8 @@ const artistInfo = {
 		},
 	},
 	lux: {
-		name: "@blittergots",
-		bio:
-		`Hi! I'm Lux Morgan. I like to play and draw :) 
+		name: "blittergots",
+		bio: `Hi! I'm Lux Morgan. I like to play and draw :) 
 		I'm a generalist artist and game designer with a love for color, texture, and character design. I love creatures and robots and goobers of any variety more than anything else. I'm so happy to be a part of SlugCon this year, and I'm looking forward to seeing you there! (Oh, and if you're interested, commissions are open!)`,
 		artworkCount: 5,
 		socials: {
@@ -85,16 +84,18 @@ const artistInfo = {
 	},
 	potatodog: {
 		name: "potato dog studio",
-		bio: "Hi everyone! My name is Gillian, and I'm the artist behind potato dog studio. It's been awhile since I've been to SlugCon, and I'm excited to come back and see everyone again. In my spare time, I like making art + comics of my mascot potato dog, along with drawing anime fan art—some examples include Frieren and Bocchi the Rock. I will also be selling some novelty items this time such as UCSC and Year of the Horse-themed post cards. Looking forward to seeing everyone again!",
+		bio:
+			"Hi everyone! My name is Gillian, and I'm the artist behind potato dog studio. It's been awhile since I've been to SlugCon, and I'm excited to come back and see everyone again. In my spare time, I like making art + comics of my mascot potato dog, along with drawing anime fan art—some examples include Frieren and Bocchi the Rock. I will also be selling some novelty items this time such as UCSC and Year of the Horse-themed post cards. Looking forward to seeing everyone again!",
 		artworkCount: 2,
 		socials: {
 			instagram: "https://www.instagram.com/potatodogstudio/",
-bluesky: "https://bsky.app/profile/potatodogstudio.bsky.social",
+			bluesky: "https://bsky.app/profile/potatodogstudio.bsky.social",
 		},
 	},
 	starlotties: {
 		name: "starlotties",
-		bio: "I'm starlotties! I'm really into Vocaloid and Utaite!! I hope to spread joy with my art :D",
+		bio:
+			"I'm starlotties! I'm really into Vocaloid and Utaite!! I hope to spread joy with my art :D",
 		artworkCount: 6,
 		socials: {
 			personal: "https://starlotties.carrd.co/",
@@ -106,8 +107,8 @@ bluesky: "https://bsky.app/profile/potatodogstudio.bsky.social",
 	},
 	rowan: {
 		name: "RowanDownAStream",
-		bio: 
-		`Hi, I am Rowan. I have been drawing a lot since middle school and a big video game fan, my favorite being The Legend of Zelda series. Currently drawing a lot of my Warrior of Light from FFXIV. 
+		bio:
+			`Hi, I am Rowan. I have been drawing a lot since middle school and a big video game fan, my favorite being The Legend of Zelda series. Currently drawing a lot of my Warrior of Light from FFXIV. 
 		I am most active on blue sky: <a href="https://bsky.app/profile/rowandownastream.bsky.social" target="_blank">https://bsky.app/profile/rowandownastream.bsky.social</a>
 		I also sell my wares on Etsy: <a href="https://rowandownastream.etsy.com" target="_blank">https://rowandownastream.etsy.com</a>`,
 		artworkCount: 6,
@@ -116,16 +117,90 @@ bluesky: "https://bsky.app/profile/potatodogstudio.bsky.social",
 			bluesky: "https://bsky.app/profile/rowandownastream.bsky.social",
 		},
 	},
+	ari: {
+		name: "Parade Animals",
+		bio:
+			"Hello I'm Ari I'm an oc and fan-artist and I like Fern Frieren and Warrior cats and Cammy Street Fighter!!",
+		artworkCount: 10,
+		socials: {
+			instagram: "https://instagram.com/parade.animals",
+		},
+	},
+	lumiekko: {
+		name: "Lumiekko",
+		bio: "I like collage, paint, and lineart. Check out my stuff <º )))><",
+		artworkCount: 5,
+		socials: {
+			instagram:
+				"https://www.instagram.com/lumiekko?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr",
+			tumblr: "https://lumiekko.tumblr.com/",
+		},
+	},
+	aiaalina: {
+		name: "aiaalina",
+		bio:
+			"Hello I’m Alina, a first year AGPM student from Merrill College! I like drawing anime characters in a bright and colorful art style.",
+		artworkCount: 10,
+		socials: {
+			twitter: "https://x.com/aiaalina?s=21",
+		},
+	},
+	em: {
+		name: "emiree_draws",
+		bio: "Howdy! My name is Em (they/them), but I go by emiree_draws! I am UCSC alumni who lives in LA! I am interested in cartoon shows, webtoons, video games, and digital art. I plan on selling MOSTLY stickers (most are waterproof except my older stock) and some leftover prints from previous events! See you there~",
+		artworkCount: 10,
+		socials: {
+			instagram:
+				"https://www.instagram.com/emiree_draws?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr",
+		},
+	},
+	cypress: {
+		name: "cypr35s",
+		bio: "Yo, I'm cyp35s! I’m a digital artist and professional enjoyer of women loving women. I mostly draw ship art (yes, it’s primarily yuri), with a focus on soft moments, dramatic feelings, and characters who probably deserve better. I’m heavily inspired by Madoka Magica, Dungeon Meshi, Look Back, and Alien Stage. If you like yearning, doomed vibes, and girls who are a little too in love, my stuff might be for you..!",
+		artworkCount: 4,
+		socials: {
+			instagram: "https://www.instagram.com/cypr35s/",
+		},
+	},
+	stella: {
+		name: "lalalumii",
+		bio: "Helloo!! My name is Stella Nguyen(@lalalumii), and I am a first year AGPM major. I am a digital artist of 8 years, and draw mostly in a painterly-like style. You will often see me snooping around projects or participating in events, that is mostly where you will see most of my art. Outside of that, I'm a fella with a wizard hat on, so don't be afraid to come and talk with me :D ",
+		artworkCount: 4,
+		socials: {
+			instagram: "https://www.instagram.com/lalalumiii__/",
+		},
+	},
+	kiiyori: {
+		name: "kiiyori_",
+		bio: "My name is Keanna Honda, I am a 2026 graduating AGPM senior! I love video games, anime, KPOP and comic books! Some of my favorites are XMEN, Batman, TWICE, ATEEZ and Resident Evil!",
+		artworkCount: 2,
+		socials: {
+			instagram: "https://www.instagram.com/kiiyori_/",
+		},
+	},
+	antzz: {
+		name: "antzz",
+		bio: "I'm antzz, and I'm a CS + (soon) AGPM major! I made digital art for things I like! *ahem* gacha games *ahem*.",
+		artworkCount: 2,
+		socials: {
+			twitter: "https://x.com/ant_zz1",
+		},
+	},
 	// name: {
 	// 	name: "",
 	// 	bio: "",
 	// 	artworkCount: 0,
 	// 	socials: {},
 	// },
-	// name: {
-	// 	name: "",
-	// 	bio: "",
+	// cabij: {
+	// 	name: "phantomCabij",
+	// 	bio:
+	// 		"I'm Marlo/Cabij, digital and traditional artist who loves making little fandom trinkets! I'm a 4th year astrophysics major, art is mainly my hobby but I like to share what I make with others and hopefully they'll like my trinkets and pngs as much as I do!!",
 	// 	artworkCount: 0,
-	// 	socials: {},
+	// 	socials: {
+	// 		tumblr: "https://www.tumblr.com/blog/phantomcabij",
+	// 		bluesky: "https://bsky.app/profile/phantomcabij.bsky.social",
+	// 		twitter: "https://x.com/phantomCabij",
+	// 	},
 	// },
 };
