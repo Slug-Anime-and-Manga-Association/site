b3e018cd74b677b21718591fbe0ccf60be0dd756
maozblan <lyssa.li137@gmail.com>
2026-05-17 14:35:46 -0700

    more artists >:0

--- slugcon2026/artists/artistInfo.js
+++ slugcon2026/artists/artistInfo.js
@@ -147,7 +147,8 @@ const artistInfo = {
 	},
 	em: {
 		name: "emiree_draws",
-		bio: "Howdy! My name is Em (they/them), but I go by emiree_draws! I am UCSC alumni who lives in LA! I am interested in cartoon shows, webtoons, video games, and digital art. I plan on selling MOSTLY stickers (most are waterproof except my older stock) and some leftover prints from previous events! See you there~",
+		bio:
+			"Howdy! My name is Em (they/them), but I go by emiree_draws! I am UCSC alumni who lives in LA! I am interested in cartoon shows, webtoons, video games, and digital art. I plan on selling MOSTLY stickers (most are waterproof except my older stock) and some leftover prints from previous events! See you there~",
 		artworkCount: 10,
 		socials: {
 			instagram:
@@ -156,7 +157,8 @@ const artistInfo = {
 	},
 	cypress: {
 		name: "cypr35s",
-		bio: "Yo, I'm cyp35s! I’m a digital artist and professional enjoyer of women loving women. I mostly draw ship art (yes, it’s primarily yuri), with a focus on soft moments, dramatic feelings, and characters who probably deserve better. I’m heavily inspired by Madoka Magica, Dungeon Meshi, Look Back, and Alien Stage. If you like yearning, doomed vibes, and girls who are a little too in love, my stuff might be for you..!",
+		bio:
+			"Yo, I'm cyp35s! I’m a digital artist and professional enjoyer of women loving women. I mostly draw ship art (yes, it’s primarily yuri), with a focus on soft moments, dramatic feelings, and characters who probably deserve better. I’m heavily inspired by Madoka Magica, Dungeon Meshi, Look Back, and Alien Stage. If you like yearning, doomed vibes, and girls who are a little too in love, my stuff might be for you..!",
 		artworkCount: 4,
 		socials: {
 			instagram: "https://www.instagram.com/cypr35s/",
@@ -164,7 +166,8 @@ const artistInfo = {
 	},
 	stella: {
 		name: "lalalumii",
-		bio: "Helloo!! My name is Stella Nguyen(@lalalumii), and I am a first year AGPM major. I am a digital artist of 8 years, and draw mostly in a painterly-like style. You will often see me snooping around projects or participating in events, that is mostly where you will see most of my art. Outside of that, I'm a fella with a wizard hat on, so don't be afraid to come and talk with me :D ",
+		bio:
+			"Helloo!! My name is Stella Nguyen(@lalalumii), and I am a first year AGPM major. I am a digital artist of 8 years, and draw mostly in a painterly-like style. You will often see me snooping around projects or participating in events, that is mostly where you will see most of my art. Outside of that, I'm a fella with a wizard hat on, so don't be afraid to come and talk with me :D ",
 		artworkCount: 4,
 		socials: {
 			instagram: "https://www.instagram.com/lalalumiii__/",
@@ -172,7 +175,8 @@ const artistInfo = {
 	},
 	kiiyori: {
 		name: "kiiyori_",
-		bio: "My name is Keanna Honda, I am a 2026 graduating AGPM senior! I love video games, anime, KPOP and comic books! Some of my favorites are XMEN, Batman, TWICE, ATEEZ and Resident Evil!",
+		bio:
+			"My name is Keanna Honda, I am a 2026 graduating AGPM senior! I love video games, anime, KPOP and comic books! Some of my favorites are XMEN, Batman, TWICE, ATEEZ and Resident Evil!",
 		artworkCount: 2,
 		socials: {
 			instagram: "https://www.instagram.com/kiiyori_/",
@@ -180,10 +184,90 @@ const artistInfo = {
 	},
 	antzz: {
 		name: "antzz",
-		bio: "I'm antzz, and I'm a CS + (soon) AGPM major! I made digital art for things I like! *ahem* gacha games *ahem*.",
+		bio:
+			"I'm antzz, and I'm a CS + (soon) AGPM major! I made digital art for things I like! *ahem* gacha games *ahem*.",
 		artworkCount: 2,
 		socials: {
 			twitter: "https://x.com/ant_zz1",
+		},
+	},
+	klq: {
+		name: "KLQ Draws",
+		bio:
+			"Hi hi! I’m Q, a UCSC alumni and local freelance illustrator and character designer. I work in a style that is a unique blend of anime and cartoon and draw pretty much anything I personally enjoy!",
+		artworkCount: 5,
+		socials: {
+			bluesky: "https://bsky.app/profile/klqdraws.bsky.social",
+		},
+	},
+	shweep: {
+		name: "orpharax.shweep",
+		bio:
+			"Hi! I’m an independent artist focusing on sculptures and paintings! Feel free to reach out for custom sculpts before the event!!",
+		artworkCount: 1,
+		socials: {},
+	},
+	chucklenutsjr: {
+		name: "Chucklenutsjr",
+		bio: "An artist dabbling in many mediums! ✍️👾🧵",
+		artworkCount: 8,
+		socials: {},
+	},
+	wildwolf: {
+		name: "Its_WildWolf",
+		bio:
+			`Hiii! :D My name is Cece (its_wildwolf), and this is my first year at Slugcon!
+I’m a fan artist who loves creating work inspired by fandoms like Genshin Impact, HSR, ALNST, and many more.
+
+I’ll be bringing a variety of merch including prints, charms, and photocards—feel free to stop by and take a look. I’d love to meet you!`,
+		artworkCount: 5,
+		socials: {
+			instagram: "https://www.instagram.com/its_wildwolf/",
+		},
+	},
+	nyanroki: {
+		name: "nyanroki",
+		bio: "Character illustrator",
+		artworkCount: 1,
+		socials: {
+			instagram: "https://www.instagram.com/nyanrokiz/",
+		},
+	},
+	henn: {
+		name: "Astro Hen",
+		bio: "hi! I’m hen, an astrophysics student who mostly draws fanart and/or original artworks of my persona.",
+		artworkCount: 8,
+		socials: {},
+	},
+	yahuri: {
+		name: "Yahuri",
+		bio: "im buny 🐇",
+		artworkCount: 9,
+		socials: {},
+	},
+	syllin: {
+		name: "Syllin Xiloscient (@silly.syllin.arts)",
+		bio: "Hello, I'm a third-year AGPM student! I love Twisted Wonderland and Genshin Impact, but I like plenty of other things (like Black Butler). My favorite characters are Malleus Draconia and Neuvillette. Sorry I don't have a lot of products to show; I'm still trying to make more stuff as I haven't sold my work much before. I hope you enjoy my art, at least!",
+		artworkCount: 3,
+		socials: {
+			instagram: "https://www.instagram.com/silly.syllin.arts?igsh=MzRlODBiNWFlZA==",
+		},
+	},
+	isabelldexigns: {
+		name: "isabelldexigns",
+		bio: "Hi hi, my name is Audrey and I am a jewelry designer and artist. I am a second year FDM and AGPM student. I am the founder of the small jewelry business, isabelldexigns. I love to create work that reflects who I am, and am so excited to share it with all of you!! :)",
+		artworkCount: 1,
+		socials: {
+			instagram: "https://www.instagram.com/Isabelldexigns/",
+		},
+	},
+	luke: {
+		name: "lukremium",
+		bio: "hi!! :D i'm luke and i love homestuck and sonic!! im still a newbie when it comes to artist alley, but i'm excited to meet everyone! i got hella sonic merch.... HELLA",
+		artworkCount: 0,
+		socials: {
+			instagram: "https://www.instagram.com/lukremium/",
+			twitter: "https://x.com/lukremium",
 		},
 	},
 	// name: {
